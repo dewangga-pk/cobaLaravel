130876f4b740d53c4fded4cee77288e16ad62e36
dewangga-pk <pacetcommunity570@gmail.com>
2020-09-17 11:26:16 +0700

    edit&update

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -81,7 +81,7 @@ class StudentsController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+        return view('students.edit',compact('student'));
     }
 
     /**
@@ -93,7 +93,18 @@ class StudentsController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'nrp' => 'required|size:9'
+        ]);
+        Student::where('id',$student->id)
+                ->update([
+                   'nama'   => $request->nama,
+                    'nrp'   => $request->nrp,
+                    'email' => $request->email,
+                    'jurusan' => $request->jurusan
+                ]);
+        return redirect('/students')->with('status','Data Mahasiswa Berhasil Diubah!');
     }
 
     /**
