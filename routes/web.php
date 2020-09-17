ce692985a82bdfc90a6ded5a07bfdea1a9cc7b15
dewangga-pk <pacetcommunity570@gmail.com>
2020-09-17 11:33:59 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,11 @@ Route::get('/about', 'PagesController@about');
 Route::get('/mahasiswa', 'MahasiswaController@index');
 
 //Route Students
-Route::get('/students','StudentsController@index');
-Route::get('/students/create','StudentsController@create');
-Route::get('/students/{student}','StudentsController@show');
-Route::post('/students','StudentsController@store');
-Route::delete('/students/{student}','StudentsController@destroy');
-Route::get('/students/{student}/edit','StudentsController@edit');
-Route::patch('/students/{student}','StudentsController@update');
+//Route::get('/students','StudentsController@index');
+//Route::get('/students/create','StudentsController@create');
+//Route::get('/students/{student}','StudentsController@show');
+//Route::post('/students','StudentsController@store');
+//Route::delete('/students/{student}','StudentsController@destroy');
+//Route::get('/students/{student}/edit','StudentsController@edit');
+//Route::patch('/students/{student}','StudentsController@update');
+Route::resource('students','StudentsController');
